8f456359fb16415bd06bef39ebc56c3f3c0dff17
Stacey <stacey6105@gmail.com>
2022-10-26 21:14:01 +0100

    Render date, add form that posts to do tasks

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+const ejs = require("ejs");
 const http = require("http");
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var userTasks = [];
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname + "/index.html"));
+
+    var date = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = date.toLocaleDateString("en-US", options);
+    var dayOptions ={weekday: "long"}
+    var dayOfWeek = date.toLocaleDateString("en-US", dayOptions);
+
+    res.render("list", {day: day, kindOfDay: dayOfWeek, userTasks: userTasks});
+
 });
+
+app.post("/", function(req, res) {
+    var userTask = req.body.taskInput;
+    userTasks.push(userTask);
+    res.redirect("/");
+})
 
 app.listen(3000, function(req, res){
     console.log("Server is running on port 3000.")
